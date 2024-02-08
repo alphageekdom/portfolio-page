746f4dc172fa3b3dfdba109f3b99b10683e4ba5b
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-02-08 12:10:53 -0800

    Fixed About Section and Added Portfolio Image Section

--- scripts.js
+++ scripts.js
@@ -1,2 +1,28 @@
 const currentDate = new Date().getFullYear();
 document.getElementById('current-year').innerText = currentDate;
+
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal
+const images = document.querySelectorAll('.image-container img');
+const modalImg = document.getElementById('modalImg');
+const captionText = document.getElementById('caption');
+
+images.forEach(function (image) {
+  image.addEventListener('click', function () {
+    modal.style.display = 'block';
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    document.body.style.overflow = 'hidden';
+  });
+});
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+  document.body.style.overflow = 'auto';
+};
